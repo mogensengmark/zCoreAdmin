b9c9606d02548ab453d072fab0cbc8c213bd9d4e
Mogens Engmark <mogens.engmark@gmail.com>
2013-01-18 20:05:48 +0100

    Registastration form functionality.

---
+++ modules/registrationForm/registrationForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * zCoreAdmin registrationForm Module.
+ **/
+
+// Load css
+wp_enqueue_style( 'registrationForm' );
+
+include_once dirname(__FILE__) . "/templates/registrationForm01.php";
+
+?>
+
+ 
+
+ 	
